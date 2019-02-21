442b4d58d823f15e1420ed2bfd3ad3b30e6d1655
konstantin0s <constantintofan85@gmail.com>
2019-02-21 19:13:02 +0100

    Add delete movies functionality

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -15,4 +15,22 @@ $(document).ready(function() {
      }
    });
  });
+
+
+ $('.delete-film').on('click', function(e) {
+  $target = $(e.target);
+  const id = $target.attr('data-id');
+  $.ajax({
+    type: 'DELETE',
+    url: '/film/'+id,
+    success: function(response) {
+      alert('Deleting movie');
+      window.location.href='/';
+    },
+    error: function(err) {
+      console.log(err);
+    }
+  });
+});
+
 });
